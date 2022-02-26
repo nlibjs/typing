656fb469b50b5fe8ef80437b7147511e62f71883
Kei Ito <kei.itof@gmail.com>
2022-02-26 14:12:08 +0900

    test: add some testes for domain name

--- src/is/String.test.ts
+++ src/is/String.test.ts
@@ -2,7 +2,20 @@ import {listCheckerTests} from './tests.private';
 import {isString} from './String';
 
 describe(isString.name, () => {
-    for (const {key, input, expected} of listCheckerTests('EmptyString', 'NonEmptyString', 'UUID', 'NonUUID')) {
+    for (const {key, input, expected} of listCheckerTests(
+        'EmptyString',
+        'NonEmptyString',
+        'UUID',
+        'NonUUID',
+        'Localhost',
+        'ExampleDotCom',
+        'DomainWithHyphenAndDigits',
+        'DomainStartsWithDigits',
+        'InvalidDomainEndsWithHyphen',
+        'InvalidDomainStartsWithDot',
+        'InvalidDomainEndsWithDot',
+        'IPv4',
+    )) {
         it(`${key} → ${expected}`, () => {
             expect(isString(input)).toBe(expected);
         });
