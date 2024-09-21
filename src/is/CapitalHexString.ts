aad6ee1ff44c9bc640114e6962e33492349c7776
Kei Ito <kei.itof@gmail.com>
2024-09-21 21:48:16 +0900

    breaking: delete some string checkers

--- src/is/CapitalHexString.ts
+++
@@ -1,6 +0,0 @@
-import { createTypeChecker } from "../createTypeChecker.ts";
-import type { Nominal, TypeChecker } from "../generics.ts";
-
-export type CapitalHexString = Nominal<string, "CapitalHexString">;
-export const isCapitalHexString: TypeChecker<CapitalHexString, RegExp> =
-	createTypeChecker<CapitalHexString>(/^[0-9A-F]*$/);
--- src/is/NumberString.ts
+++
@@ -1,7 +0,0 @@
-import { createTypeChecker } from "../createTypeChecker.ts";
-import type { Nominal, TypeChecker } from "../generics.ts";
-
-export const NumericCharacters = "0123456789";
-export type NumberString = Nominal<string, "NumberString">;
-export const isNumberString: TypeChecker<NumberString, RegExp> =
-	createTypeChecker<NumberString>(new RegExp(`^[${NumericCharacters}]*$`));
