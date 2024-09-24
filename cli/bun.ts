4111120d713a301eaf1c1c7c76b1b8dfaf9c2c3e
Kei Ito <kei.itof@gmail.com>
2024-09-25 02:15:12 +0900

    ci: run tests with bun

--- cli/bun.ts
+++ cli/bun.ts
@@ -4,16 +4,15 @@ import { listFiles, listTsSourceStringLiterals } from "./util.ts";
 import * as fs from "node:fs/promises";
 
 const srcDir = new URL("../src/", import.meta.url);
-const tempDir = new URL("../temp/", import.meta.url);
-const exclude = [/\.test\..*$/];
+const bunDir = new URL("../bun/", import.meta.url);
 
-await fs.mkdir(tempDir, { recursive: true });
+await fs.mkdir(bunDir, { recursive: true });
 
-for await (const fileUrl of listFiles(tempDir)) {
+for await (const fileUrl of listFiles(bunDir)) {
 	await fs.unlink(fileUrl);
 }
 
-for await (const fileUrl of listFiles(srcDir, exclude)) {
+for await (const fileUrl of listFiles(srcDir)) {
 	const filePath = fileUrl.pathname.slice(srcDir.pathname.length);
 	if (filePath.endsWith(".ts")) {
 		let code = await fs.readFile(fileUrl, "utf-8");
@@ -23,10 +22,9 @@ for await (const fileUrl of listFiles(srcDir, exclude)) {
 			ts.ScriptTarget.ESNext,
 		);
 		const replaceList: Array<ts.StringLiteral> = [];
-		const ext = ".ts";
 		for (const node of listTsSourceStringLiterals(tsSource)) {
 			const importFromText = node.text;
-			if (importFromText.startsWith(".") && importFromText.endsWith(ext)) {
+			if (importFromText === "node:test") {
 				replaceList.push(node);
 			}
 		}
@@ -34,9 +32,9 @@ for await (const fileUrl of listFiles(srcDir, exclude)) {
 		for (const node of replaceList) {
 			const start = code.indexOf(node.text, node.getFullStart());
 			const end = start + node.text.length;
-			code = `${code.slice(0, end - ext.length)}.js${code.slice(end)}`;
+			code = `${code.slice(0, start)}bun:test${code.slice(end)}`;
 		}
-		const destUrl = new URL(filePath, tempDir);
+		const destUrl = new URL(filePath, bunDir);
 		await fs.mkdir(new URL(".", destUrl), { recursive: true });
 		await fs.writeFile(destUrl, code);
 	}
